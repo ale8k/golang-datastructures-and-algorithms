209c13077636375ee5a0f31f55d155237983699c
ale8k <alexanderkilroy@outlook.com>
2021-12-02 21:55:36 +0000

    BubbleSort on ints
     Would like to learn more about go reflection to pass basically 'any value'
     and sort it. Even if the struct field key has to be specified.

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	linkedlist "github.com/ale8k/golang_datastructures_and_algorithms/data_structures/linked_list"
-)
-
 func main() {
-	list := &linkedlist.LinkedList{}
-	list.Append(10)
-	list.Append(20)
-	list.Append(30)
 
-	val, err := list.Get(1)
-	fmt.Println(val, err)
 }
---
+++ slice_sorting/bubble_sort/bubble_sort.go
@@ -0,0 +1,19 @@
+package bubblesort
+
+// Sorts a slice of integers using sinking sort algorithm
+func BubbleSortInt(slice []int) {
+	swapWithNextIndex := func(i int, slice []int) {
+		temp := slice[i+1]
+		slice[i+1] = slice[i]
+		slice[i] = temp
+	}
+	for i := range slice {
+		for j := range slice {
+			if i != len(slice)-1 && j != len(slice)-1 {
+				if slice[j] > slice[j+1] {
+					swapWithNextIndex(j, slice)
+				}
+			}
+		}
+	}
+}
